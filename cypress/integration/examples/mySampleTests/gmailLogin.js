38bc5ba9ff44d8e2919efabe53f55a91adb3bd71
opprana <opprana@amazon.com>
2021-05-19 17:01:54 +0530

    am commit

---
+++ cypress/integration/examples/mySampleTests/gmailLogin.js
@@ -0,0 +1,33 @@
+import AccountManagementPage from '../PageObject/AccountManagementPage'
+
+describe('Gmail Test ' , function() {
+
+   /* before(function(){
+        cy.fixture('logindata').then(function(data) {
+            this.data=data
+
+        })
+        
+
+    }) */
+
+
+
+    it('login check' , function() {
+
+       // cy.visit('http://login.bamboobox.in/#/')  
+       cy.visit('http://b2prospect.bamboobox.in/')
+      
+       // cy.get('button').click()
+      // cy.get('#identifierId').
+
+       
+
+       
+
+        
+    })
+
+})  
+
+ 
---
+++ cypress/integration/examples/mySampleTests/myAmazonLogin.js
@@ -0,0 +1,10 @@
+describe('Amazon Test ' , ()=> {
+
+    it('login check' , ()=> {
+
+        cy.visit('http://login.bamboobox.in/')  //opens url
+        cy.get('span')
+        
+    })
+
+})  

7afc3ed487803085d2f2049fd16622fc4cf7a8ad
opprana <opprana@amazon.com>
2021-05-20 14:09:42 +0530

    contactmanagement

--- cypress/integration/examples/mySampleTests/gmailLogin.js
+++ cypress/integration/examples/mySampleTests/gmailLogin.js
@@ -1,34 +1,12 @@
-import AccountManagementPage from '../PageObject/AccountManagementPage'
+
 
 
 
 
 describe('Gmail Test ' , function() {
 
-   
-
-
-
     it('AccountCreationTest' , function() {
         cy.visit('http://b2prospect.bamboobox.in/')
-        const account = new AccountManagementPage() 
-        
-
-      // cy.visit('http://b2prospect.bamboobox.in/')
-      
-
-       
-
-
-
-
-      
-       
-
-       
-
-       
-
         
     })
 
---
+++ cypress/support/PageObject/ContactManagementPage.js
@@ -0,0 +1,39 @@
+class ContactManagementPage {
+
+    ClickToggle() {
+        cy.get('.jss6').click()
+    }
+
+    FillCompanyWebsite(CompanyWebsiteValue) {
+        cy.get('.rgt-cell-companyWebsite').type(CompanyWebsiteValue)
+    }
+
+    FillCompanyName(CompanyNameValue) {
+        cy.get('.rgt-cell-companyWebsite > input').type(CompanyNameValue)
+    }
+
+    FillFirstName(FitrstNameValue) {
+        cy.get('.rgt-cell-contactFirstName').type(FitrstNameValue)
+    }
+
+    FillLastName(LastNameValue) {
+        cy.get('.rgt-cell-contactLastName > .rgt-cell-inner > .rgt-cell-editor-inner > .rgt-cell-editor-input').type(LastNameValue)
+    }
+
+    FillOfficialEmail(OfficialEmailValue) {
+        cy.get('.rgt-cell-contactEmail').type(OfficialEmailValue)
+    }
+
+    FillLinkedinURL(LinkedinURLValue) {
+        cy.get('.rgt-cell-contactLinkedin').type(LinkedinURLValue)
+    }
+
+    CreateContact() {
+        cy.get('[title="Save"] > svg').click()
+    }
+
+    
+
+}
+
+export default ContactManagementPage

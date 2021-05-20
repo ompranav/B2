dbedd55b52c7ea3ba0dbebdd0dfdb80d6bc2f635
shreyapanale <shreya.panale@gmail.com>
2021-05-20 14:34:26 +0530

    AMData

--- cypress/integration/examples/accountManagement.test.js
+++ cypress/integration/examples/accountManagement.test.js
@@ -3,19 +3,22 @@ import AccountManagementPage from '../../support/PageObject/AccountManagementPag
 describe('AM tests',function(){
     before('visit website',() =>{
         cy.visit('http://b2prospect.bamboobox.in/')
+        cy.fixture('AMData').then((data)  => {
+            this.data=data
+        })
     })
     it('AM Account Creation',() => {
         const AM = new AccountManagementPage();
-        AM.FillCompanyName('abcd')
-        AM.FillCompanyWebsite('abcd.com')
+        AM.FillCompanyName(this.data.C1.companyName)
+        AM.FillCompanyWebsite(this.data.C1.websiteName)
         AM.CreateAccount()
         cy.contains('has been added/edited successfully!')
-        cy.contains('abcd').should('exist')
+        cy.contains(this.data.C1.companyName).should('exist')
     })
     it("Doesn't allow Duplicate account creation", () =>{
         const AM = new AccountManagementPage();
-        AM.FillCompanyName('abcd')
-        AM.FillCompanyWebsite('abcd.com')
+        AM.FillCompanyName(this.data.C1.companyName)
+        AM.FillCompanyWebsite(this.data.C1.websiteName)
         AM.CreateAccount()
         cy.on('window:alert', (str) => {
             expect(str).to.equal(`Entered Company Website is already present in the grid.`)
@@ -24,7 +27,7 @@ describe('AM tests',function(){
     })
     it('Mandatory Fields Error Message Displayed Correctly',()=>{
         const AM = new AccountManagementPage();
-        AM.FillCompanyName('try')
+        AM.FillCompanyName(this.data.C2.companyName)
         AM.CreateAccount()
         cy.on('window:alert', (str) => {
             expect(str).to.equal(`No Mandatory fields passed`)
